50ec98999dd48e055a7591eaa47ce68190c11a23
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-04-10 10:55:31 +0630

    config appliction.rb

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,5 @@
 class RootController < ApplicationController
     def index
-        render json: {welcome: "hello"}
+        render json: {message: "hello"}
     end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module BookingBackendApi
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' 
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
