ab99febcbb23959e2637afd0aa87d8f2a9434743
jorgegoco <jorgegoco70@gmail.com>
2023-04-10 18:35:39 +0200

    Create routes for reservations

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "root#index"
+  namespace :api do
+    namespace :v1 do
+      resources :reservations, only: [:index, :create, :destroy]
+    end
+  end
 end
