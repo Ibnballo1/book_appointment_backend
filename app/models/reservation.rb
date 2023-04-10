dc0e8367c34e9801704643f8f3d9f618c6e1ec4d
jorgegoco <jorgegoco70@gmail.com>
2023-04-10 20:01:16 +0200

    Add validation to dates on reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,20 @@
 class Reservation < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :room, class_name: 'Room', foreign_key: 'room_id'
+
+  validate :validate_dates
+
+  def validate_dates
+    if start_date.present? && !start_date.is_a?(Date)
+      errors.add(:start_date, 'must be a valid date')
+    end
+    
+    if end_date.present? && !end_date.is_a?(Date)
+      errors.add(:end_date, 'must be a valid date')
+    end
+    
+    if start_date.present? && end_date.present? && end_date < start_date
+      errors.add(:end_date, 'cannot be before start date')
+    end
+  end
 end
