84e613629595c68a3232d60188b76551e993cd19
jorgegoco <jorgegoco70@gmail.com>
2023-04-10 18:58:26 +0200

    Show reservations for current user

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.where(user_id: @current_user.id)
+    render json: reservations
+  end
 end
