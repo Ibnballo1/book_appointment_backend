a4586b0120044f81170ba600027c8913b818359f
Ibnballo1 <belloabdullateef035@gmail.com>
2023-04-11 22:31:45 +0100

    Add rooms controller to v1 folder

---
+++ app/controllers/api/v1/rooms_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::RoomsController < ApplicationController
+  before_action :set_room, only: [:show, :update, :destroy]
+
+  # GET /api/rooms
+  def index
+    @rooms = Room.all
+    render json: @rooms
+  end
+
+  # GET /api/rooms/:id
+  def show
+    render json: @room
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      render json: @room, status: :created
+    else
+      render json: @room.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/rooms/:id
+  def update
+    if @room.update(room_params)
+      render json: @room
+    else
+      render json: @room.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/rooms/:id
+  def destroy
+    @room.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :description, :photo, :city, :price)
+  end
+end
