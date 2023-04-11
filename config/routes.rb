5e064fae6e7564a8ca861213fd5dce77f90cbac5
Ibnballo1 <belloabdullateef035@gmail.com>
2023-04-11 22:34:28 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :api do
-    resources :rooms
-  end
+  # namespace :api do
+  #   resources :rooms
+  # end
   # Defines the root path route ("/")
   root "root#index"
   namespace :api do
     namespace :v1 do
       resources :reservations, only: [:index, :create, :destroy]
+      resources :rooms
     end
   end
 end
