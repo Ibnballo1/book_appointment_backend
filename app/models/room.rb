9ca2d7aa2a8e05a35d5b96493a9f8fa534e3eea3
jorgegoco <jorgegoco70@gmail.com>
2023-04-12 11:26:04 +0200

    Delete belongs to user for room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,5 @@
 class Room < ApplicationRecord
   has_many :reservations
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   
   validates :name, presence: true, length: { maximum: 255 }
   validates :description, presence: true
