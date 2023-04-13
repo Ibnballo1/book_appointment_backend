1f6d48ae45e201cfe954302479f958caef496a47
jorgegoco <jorgegoco70@gmail.com>
2023-04-13 20:27:20 +0200

    Fix seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-User.create([
-  {name: 'Abdullaah'},
-  {name: 'Raslaan'},
-  {name: 'Aaisha'},
-])
-
 Room.create([
   {name: 'Room 1', description: 'yellow colored room', photo: 'photo.png', city: 'Lagos', price: 500},
   {name: 'Room 2', description: 'white colored room', photo: 'photo.png', city: 'Kwara', price: 230},
   {name: 'Room 3', description: 'green colored room', photo: 'photo.png', city: 'Abuja', price: 1350},
-])
-
-Reservation.create([
-  {start_date: '17-03-2012', end_date: '15-04-2012', room_id:3, user_id: 2},
 ])
