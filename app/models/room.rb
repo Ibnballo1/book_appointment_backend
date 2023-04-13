5bad527b858c77de664f2458b3d718cae5f84baa
jorgegoco <jorgegoco70@gmail.com>
2023-04-13 18:20:34 +0200

    Fix delete reservations for deleted room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :description, presence: true
