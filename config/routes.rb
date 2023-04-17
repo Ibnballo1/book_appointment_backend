a8208ea949d840166d68819ba65f0978aac4af5b
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2023-04-17 15:51:35 +0300

    Add details route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :reservations, only: [:index, :create, :destroy]
-      resources :rooms
+      resources :rooms, only: [:index, :show]
     end
   end
   resources :users
