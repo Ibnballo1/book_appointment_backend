e27bd7db005cf1d52aa692b996c3e34811ff21b2
FuadNabi <fuad.nebiyev95@mail.ru>
2023-04-18 15:36:12 +0400

    Add test, Reservation Api should contain end date

--- spec/unit/reservations_spec.rb
+++ spec/unit/reservations_spec.rb
@@ -18,4 +18,16 @@ RSpec.describe Reservation do
                                       room_id: room.id)
     expect(reservation.start_date.strftime('%a, %d %b %Y')).to eq('Fri, 19 May 2023')
   end
+
+  it 'Reservation Api should contain end date' do
+    user = User.create!(name: 'admin', email: 'admin@gmail.com', password: '123456')
+    room = Room.create(name: 'Name',
+                       photo: 'photo.png',
+                       price: '100',
+                       city: 'City',
+                       description: 'Description')
+    reservation = Reservation.create!(start_date: '2023/05/19', end_date: '2023/05/20', user_id: user.id,
+                                      room_id: room.id)
+    expect(reservation.end_date.strftime('%a, %d %b %Y')).to eq('Sat, 20 May 2023')
+  end
 end
