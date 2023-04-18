7303db4a6a9e701cce6b8804ee2392b25b01b28c
FuadNabi <fuad.nebiyev95@mail.ru>
2023-04-18 15:48:03 +0400

    Add test, User should have a name

--- spec/models/user.rb
+++ spec/models/user.rb
@@ -7,4 +7,11 @@ RSpec.describe User do
                        password: '')
     expect(user).to_not be_valid
   end
+  
+  it 'User should have a name' do
+    user = User.create(name: 'Admin',
+                       email: 'Admin@email.com',
+                       password: '123123')
+    expect(user.name).to eql('Admin')
+  end
 end
