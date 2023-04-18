3305ca59a023d49c0b6b026577e296e471089a8a
FuadNabi <fuad.nebiyev95@mail.ru>
2023-04-18 15:34:41 +0400

    Add test, Reservation Api should contain start date

--- spec/unit/reservations_spec.rb
+++ spec/unit/reservations_spec.rb
@@ -1,9 +1,21 @@
 require 'swagger_helper'
 
 RSpec.describe Reservation do
-  it 'should be valid' do
+  it 'Reservation Api should be valid' do
     reservation = Reservation.create(start_date: Date.today,
                                      end_date: Date.tomorrow)
     expect(reservation).to_not be_valid
+  end
+
+  it 'Reservation Api should contain start date' do
+    user = User.create!(name: 'admin', email: 'admin@gmail.com', password: '123456')
+    room = Room.create(name: 'Name',
+                       photo: 'photo.png',
+                       price: '100',
+                       city: 'City',
+                       description: 'Description')
+    reservation = Reservation.create!(start_date: '2023/05/19', end_date: '2023/05/20', user_id: user.id,
+                                      room_id: room.id)
+    expect(reservation.start_date.strftime('%a, %d %b %Y')).to eq('Fri, 19 May 2023')
   end
 end
