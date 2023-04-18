98c18df7d11d4d05cb93480e8d0fa74d50a474da
FuadNabi <fuad.nebiyev95@mail.ru>
2023-04-18 15:20:03 +0400

    Add test, Room Api should have a photo

--- spec/unit/rooms_spec.rb
+++ spec/unit/rooms_spec.rb
@@ -1,17 +1,6 @@
 require 'swagger_helper'
 
 RSpec.describe Room do
-  # let(:user) do
-  #   User.create(name: 'Admin', email: 'admin@gmail.com', password: '123456789', role: 'admin')
-  # end
-
-  # let(:token) do
-  #   post '/auth/login', params: { email: user.email, password: user.password }
-  #   response.headers['Authorization']
-  # end
-
-  # let(:auth_headers) { { 'Authorization': "Bearer #{token}" } }
-
   it 'Room Api should be valid' do
     room = Room.create(name: '',
                        photo: '',
@@ -28,5 +17,14 @@ RSpec.describe Room do
                        city: 'City',
                        description: 'Description')
     expect(room.name).to eql('Name')
+  end
+  
+  it 'Room Api should have a photo' do
+    room = Room.create(name: 'Name',
+                       photo: 'photo.png',
+                       price: '100',
+                       city: 'City',
+                       description: 'Description')
+    expect(room.photo).to eql('photo.png')
   end
 end
